3b880d0ae94b6e496e1f2f0440f7e41cdbb588d5
Rishab Dugar <www.anitadugar9163@gmail.com>
2022-12-13 19:04:49 +0530

    fixed design issues

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ backToTop.addEventListener('click', () => {
 	window.scrollTo(0, 0)
 })
 
+let galleryLeft = 0;
+let disableRight = 1;
+let disableleft = 0;
+const windowInnerWidth = window.innerWidth;
+// const width = 
+
+
+// const onLeft = () => {
+// 	if (galleryLeft < 0) {
+// 		galleryLeft += 100;
+// 		disableRight = 0;
+// 	}
+// 	else {
+// 		disableleft = 1;
+// 	}
+// }
+// const onRight = () => {
+// 	if(+windowInnerWidth - left < )
+// }
